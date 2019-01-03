b669d8ae476721fc8ce94fe948faf134aa4a3aad
Calie Rushton <calielrushton@gmail.com>
2019-01-03 11:45:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,12 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array.push yield array[i]
+    new_array.push yield (array[i])
     # yield element to the block and put the result in a new array
     i = i + 1
   end
   new_array
 end
+
+# syntax differences with .push and <<
+# if using <<, its necessary to wrap array[i] in () and remove space after yield
