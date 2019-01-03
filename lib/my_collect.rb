30d4956da2ac332523dbf4f684142fceeca07466
Calie Rushton <calielrushton@gmail.com>
2019-01-03 11:35:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    yield array[i]
-    new_array << array[i]
+    new_array << yield array[i]
+    # yield element to the block and put the result in a new array
     i = i + 1
   end
   new_array
